df9efb0390d5ccfd8728c9537f8ac50fd68efa8f
Matthew Muñoz <matthew.munoz78u@std.uni.edu.ni>
2024-11-24 19:29:38 -0600

    DataSet Realizado

--- ADOConnect/Program.cs
+++ ADOConnect/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace ADOConnect
 {
@@ -8,24 +9,22 @@ namespace ADOConnect
         {
             string connectionString = "Server=MATT\\MSSQLSERVER01;Database=Northwind;Trusted_Connection=True;TrustServerCertificate=True";
 
-            string insertQuery = "INSERT INTO Products (ProductName, UnitPrice, UnitsInStock) VALUES (@ProductName, @UnitPrice, @UnitsInStock)";
+            DataSet dataSet = new DataSet();
+
+            string Query = "sELECT * FROM Products;SELECT * FROM CAtegories";
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                SqlCommand command = new SqlCommand(insertQuery, connection);
-                command.Parameters.AddWithValue("@ProductName", "New Product");
-                command.Parameters.AddWithValue("@UnitPrice", 15.00);
-                command.Parameters.AddWithValue("@UnitsInStock", 50);
+                SqlDataAdapter adapter = new SqlDataAdapter(Query, connection);
 
-                try
+                adapter.Fill(dataSet);
+
+                DataTable productsTable = dataSet.Tables[0];
+                DataTable categoriesTable = dataSet.Tables[1];
+
+                foreach (DataRow row in productsTable.Rows)
                 {
-                    connection.Open();
-                    int rowsAffected = command.ExecuteNonQuery();
-                    Console.WriteLine($"{rowsAffected} fila(s) insertada(s)");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Erorr: {ex.Message}");
+                    Console.WriteLine($"ID: {row["ProductID"]}, Nombre: {row["ProductName"]}");
                 }
             }
         }
